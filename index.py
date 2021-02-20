1ebbe774902e3b607756745df38968560d3450ce
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2021-02-20 15:17:01 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,19 @@
 from browser import document
 
-document <= "Hello !"
+document <= "File: "
+
+f = open("database.txt","w+")
+f.write("It worked!!!!!")
+f.close()
+
+
+document <= "File Edited and Closed"
+
+r = open("database.txt","r")
+for line in r:
+  data = r.read(line)
+  print(data)
+
+r.close()
+
+document <= "File Read and Closed"
